e07fff8dbe16b3966b183dc61b9347daa9c66555
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2024-05-26 23:56:25 +0200

    Added regular xUnit test project to see if that also runs twice. But it doesn't. So only Selenium somehow causes VS to load each browser window twice.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var a = 1;
+
+            Assert.Equal(1, a);
+
+        }
+    }
+}
